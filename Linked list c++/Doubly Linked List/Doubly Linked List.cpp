dfeeec86d36cc927fcff6fb8bb0acf62cc988e76
luongd123 <dungpubgame@gmail.com>
2025-01-20 13:45:41 +0700

    Double linked list code added

--- Linked list c++/Doubly Linked List/Doubly Linked List.cpp
+++ Linked list c++/Doubly Linked List/Doubly Linked List.cpp
@@ -3,13 +3,138 @@ using namespace std;
 using ll = long long;
 const ll MOD = 1e9 + 7;
 
+struct Node {
+    int x;
+    Node *next;
+    Node *prev;
+    Node(int x) {
+        this->x = x;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
 
+int size(Node *head) {
+   int cnt = 0;
+   while (head != NULL) {
+      ++cnt;
+      head = head->next;
+   }
+   return cnt;
+}
 
+void Display(Node *head) {
+    while (head != NULL) {
+        cout << head->x <<" ";
+        head = head->next;
+    }
+}
 
+Node *makeNode(int x) {
+    return new Node(x);
+}
 
+void insertFront(Node *&head, int x) {
+    Node *tmp = makeNode(x);
+    if (head == NULL) {
+        head = tmp;
+    }
+    else {
+        tmp->next = head;
+        head->prev = tmp;
+        head = tmp;
+    }
+}
 
+void insertBack(Node *&head, int x) {
+    Node *tmp = makeNode(x);
+    if (head == NULL) {
+        head = tmp;
+    }
+    else {
+        Node *p = head;
+        while(p->next != NULL){
+            p = p->next;
+        }
+        p->next = tmp;
+        tmp->prev = p;
+    }
+}
 
+void insertMiddle(Node *&head, int x, int pos) {
+    int n = size(head);
+    if (pos < 1 || pos > n + 1) { 
+        return ;
+    }
+    else {
+        if (pos == 1) {
+            insertFront(head, x);
+        }
+        else{
+            Node *p = head;
+            Node *tmp = makeNode(x);
+            for (int i = 1; i <= pos - 1;i ++){
+                p = p->next;
+            }
+            tmp->next = p;
+            p->prev->next = tmp;
+            tmp->prev = p->prev;
+            p->prev = tmp;
+        }
+    }
+}
 
+void deleteFront(Node *&head) {
+    if (head == NULL) {
+        return ;
+    }
+    Node *tmp = head;
+    head = head->next;
+    head->prev = NULL;
+    delete tmp;
+}
+
+void deleteBack(Node *&head) {
+    if (head == NULL) { 
+        return;
+    }
+    if (head->next == NULL) { 
+        delete head;
+        head = NULL; 
+        return;
+    }
+    Node *del = head;
+    while (del->next != NULL) {
+        del = del->next;
+    }
+    del->prev->next = NULL; 
+    delete del; 
+}
+
+void deleteMiddle(Node *&head, int pos) {
+    if (head == NULL) {
+        return;
+    }
+    int n = size(head); 
+    if (pos < 1 || pos > n) {
+        return;
+    }
+    if (pos == 1) {
+        deleteFront(head); 
+        return;
+    }
+    Node *p = head;
+    for (int i = 1; i < pos; i++) {
+        p = p->next;
+    }
+    if (p->prev != NULL) {
+        p->prev->next = p->next;
+    }
+    if (p->next != NULL) {
+        p->next->prev = p->prev;
+    }
+    delete p;
+}
 
 
 
@@ -55,31 +180,30 @@ int main(){
     return 0;
 }
 
-// TRY YOUR OWN INPUT : DDD
-
-/* SAMPLE INPUT:
+/*
+SAMPLE INPUT:
 20
-2 1
-2 2 
-2 3
-2 4
-1 5
-1 6
-1 7
-1 8
-1 9
-1 123
-3 12 4
-3 22 6
-3 209 8
-4
-5
-5
-6 1
-6 6
-7 
-*/
-/* 
+1 1 -> 1
+1 2 -> 2 1
+1 100 -> 100 2 1
+1 200 -> 200 100 2 1
+3 90 2 -> 200 90 100 2 1
+4 -> 90 100 2 1
+1 3 -> 3 90 100 2 1
+1 4 -> 4 3 90 100 2 1
+2 5 -> 4 3 90 100 2 1 5
+2 6 -> 4 3 90 100 2 1 5 6
+2 7 -> 4 3 90 100 2 1 5 6 7
+3 8 3 -> 4 3 8 90 100 2 1 5 6 7
+3 9 6 -> 4 3 8 90 100 9 2 1 5 6 7
+4 -> 3 8 90 100 9 2 1 5 6 7
+4 -> 8 90 100 9 2 1 5 6 7
+5 -> 8 90 100 9 2 1 5 6 
+5 -> 8 90 100 9 2 1 5 
+6 2 -> 8 100 9 2 1 5
+6 1 -> 100 9 2 1 5
+7
+
 SAMPLE OUTPUT:
-123 9 8 7 6 5 1 2 3 4 
+100 9 2 1 5 
 */
